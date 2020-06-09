12213ce1c18a9f65e1951bf0a115259f05033cad
Miguel Angel <miguelapb35@gmail.com>
2020-06-08 22:09:22 -0500

    Updates

---
+++ precache-manifest.68ba6d1e3dba6991db67428513692895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513fb54b11480f08c3e6b0029fc1b344",
+    "url": "/react-spring-turbulence/index.html"
+  },
+  {
+    "revision": "661d92640d179f30f568",
+    "url": "/react-spring-turbulence/static/css/main.fd804a78.chunk.css"
+  },
+  {
+    "revision": "35e6dbd06e65310690b3",
+    "url": "/react-spring-turbulence/static/js/2.d3e60998.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-spring-turbulence/static/js/2.d3e60998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661d92640d179f30f568",
+    "url": "/react-spring-turbulence/static/js/main.a9672839.chunk.js"
+  },
+  {
+    "revision": "c063b7171d5a12303020",
+    "url": "/react-spring-turbulence/static/js/runtime-main.f01a59c5.js"
+  }
+]);
